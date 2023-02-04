60a10d951681145b290f844876f1d13d6cb9d984
Mehedi Hasan <mhjibon.info@gmail.com>
2023-02-05 02:13:48 +0600

    comparsion js complete

---
+++ js-array-conditionals/comparsion.js
@@ -0,0 +1,34 @@
+// console.log(5 < 7);
+// console.log(5 > 7);
+
+
+// console.log( 5 == 6);
+// console.log(6 == 6);
+
+// console.log(6 != 7);
+// console.log(7 != 7);
+
+// less than or equal 
+// console.log(7 <= 6);
+// console.log(6 <= 6);
+// console.log(16 <= 6);
+// console.log(5 <= 6);
+
+// greater than or equal 
+// console.log(5 >= 6);
+// console.log(5 >= 2);
+//  console.log(2 >= 2);
+
+var num1 = 56;
+var num2 = 76;
+console.log(num1 < num2);
+console.log(num1 > num2);
+console.log(num1 == num2);
+console.log(num1 <= num2);
+console.log(num1 >= num2);
+console.log(num1 !== num2);
+
+// multiple condition
+// money1 >  money2 && result1 > result2 && height1 > height2 
+money1 >  money2 || result1 > result2 || height1 > height2 
+
