40d06ed99b9588040bc08635ee7eb722a80cf1dd
Mehedi Hasan <mhjibon.info@gmail.com>
2023-02-05 00:10:16 +0600

    types variable js

--- introduction-to-javascript/first.js
+++
@@ -1,6 +0,0 @@
-console.log(555);
-
-let shoe = 1200;
-let belt = 800;
-let totalPrice = shoe + belt; 
-console.log(totalPrice);
--- introduction-to-javascript/remainder.js
+++
@@ -1,6 +0,0 @@
-var mangos = 7;
-var person = 2;
-// /this means vagfol
-// %this means vagses 
-var remainder = mangos % person;
-console.log(remainder);
