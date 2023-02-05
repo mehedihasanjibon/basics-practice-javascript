5a073a1c55f9591340d4a52de2605c38c9ae534a
Mehedi Hasan <mhjibon.info@gmail.com>
2023-02-05 17:26:07 +0600

    array and index js complate

---
+++ array.js
@@ -0,0 +1,9 @@
+var friendsAge = [11, 21, 45, 17, 14];
+var picnicFee = [5000, 2000, 1000, 500];
+
+var nayikas = ["mahi" , "opu" , "sabnoor" , "sabana"];
+var vowels = ["a" , "e" , "i" , "o" , "u"];
+
+// console.log(picnicFee.length);
+
+console.log(6 >= 6);
---
+++ index.js
@@ -0,0 +1,14 @@
+var numbers = [45, 32, 47, 76, 87, 99];
+
+// get Element by index 
+var element = numbers[1];
+console.log(element);
+
+// set element value by index 
+numbers[1] = 77;
+numbers[3] = 44;
+console.log(numbers);
+
+// find index of an element 
+var positionIndex = numbers.indexOf(87);
+console.log(positionIndex);
