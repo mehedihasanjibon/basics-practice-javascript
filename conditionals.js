ae6cb3ec1ef493e3c862686d5d58f2087434c4e7
Mehedi Hasan <mhjibon.info@gmail.com>
2023-02-05 17:48:28 +0600

    conditionals js complate

---
+++ conditionals.js
@@ -0,0 +1,41 @@
+var iphonePrice = 79000;
+var myBudget = 95000;
+
+// if iphone price is less the my budget 
+/***
+ * if iphone price is less the my budget 
+ * i will buy the iphone
+ * 
+ * 
+ * if(condition){
+ *      will execute if the condition is true 
+ * }
+ * 
+ * */
+
+if(iphonePrice < myBudget){
+    console.log("iphone kine futani marbo");
+}
+
+var chickenPrice = 500;
+var myMoney = 50;
+
+    // if(chickenPrice <= myMoney){
+    //     console.log("murgir ranna khamu and haddi chibamu!!");
+    // }
+    // if(chickenPrice > myMoney){
+    //     console.log("smashed potato and lentils soup");
+    // }
+
+    /**
+     * if chicken er price is less than my budget 
+     * i will eat chicken
+     * na hoi: else
+     * **/
+
+    if(chickenPrice < myMoney){
+        console.log("I will eat chicken");
+    }
+    else{
+        console.log("I will eat potato");
+    }
