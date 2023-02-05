9c707a511f9deb533415077225bf48a68b948e7a
Mehedi Hasan <mhjibon.info@gmail.com>
2023-02-05 22:53:33 +0600

    array-loop js compalate

---
+++ loops-practice-problems/array-loop.js
@@ -0,0 +1,14 @@
+// target : display every elements of an array 
+var numbers = [45, 43, 76, 23, 98, 77, 65, 345, 87];
+
+// for(var i = 0; i < numbers.length; i++){
+//     var number = numbers[i];
+//     console.log(number);
+// }
+
+var items = ["bottle" , "sunglass" , "Moues" , "Pen" , "NoteBook"];
+for(var i = 0; i < items.length; i++){
+    var item = items[i];
+    console.log(item);
+
+}
