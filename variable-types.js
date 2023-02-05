bfa8db2c1d9c301056cd58210e606bae9fc95887
Mehedi Hasan <mhjibon.info@gmail.com>
2023-02-05 17:14:20 +0600

    variable-types js complate

---
+++ variable-types.js
@@ -0,0 +1,32 @@
+// number type variable
+var sunglassPrice = 120;
+
+// string type variable
+var specialOneName = "Ariana Grande";
+
+// Boolean type variable
+var isSingle = true;
+
+// two types of number: Integer: 120,
+// float, decimal: 3.65,
+
+// variable naming convention : rules, best practice
+
+/***
+ * 1. single word 
+ * 2. no gap or speace
+ * 3. no keyword 
+ * 4. no quote
+ * 5. can not start with a number 
+ * 6. can not contain dash
+ * 7. should use camelCase
+ * **/
+
+// math operations: +, -, *, /
+// shorthand : += , -= , ++ , --
+
+// convert: parseInt , parseFloat
+
+// toFixed
+
+// remainder : vagses %;
