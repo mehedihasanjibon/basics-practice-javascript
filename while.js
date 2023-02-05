d56bfdb32549a90db8c54233d16f3fea0fd9c4c8
Mehedi Hasan <mhjibon.info@gmail.com>
2023-02-05 21:18:26 +0600

    while js complate

---
+++ while.js
@@ -0,0 +1,19 @@
+// if(condition){
+
+// }
+
+/* 
+    1. Loop variable
+    2. condition inside while
+    3. loop body
+    4. change the Loop
+*/
+
+var roastGiven = 0;
+while(roastGiven < 7){
+    console.log("roast den, Please!!");
+    // roastGiven = roastGiven + 1;
+    // roastGiven += 1;
+    console.log(roastGiven);
+    roastGiven++;
+}
