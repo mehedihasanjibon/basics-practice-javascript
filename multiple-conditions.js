b9c3058cc1b8bed7d427c300629f787e30af316f
Mehedi Hasan <mhjibon.info@gmail.com>
2023-02-05 18:02:46 +0600

    multiple condition js complate

---
+++ multiple-conditions.js
@@ -0,0 +1,35 @@
+// var isGraduated = true;
+// var salary = 75000;
+// var cars = 1;
+
+// if(isGraduated == true){
+//     console.log("Aso biye kre peli");
+// }
+// else{
+//     console.log("Tor kopale biye nai");
+// }
+
+// if(isGraduated == true && salary > 50000){
+//     console.log("aso biye kre peli")
+// }
+// else{
+//     console.log("Tor kopale biye nai");
+// }
+
+// if(isGraduated == true && salary > 50000 && cars >= 1){
+//     console.log("Aso biye kre peli");
+// }
+// else{
+//     console.log("Tor kopale biye nai");
+// }
+
+var isGraduated = false;
+var salary = 50000;
+var cars = 1;
+
+if(isGraduated == true || salary > 50000 || cars >= 1){
+    console.log("Eso prem kri");
+}
+else{
+    console.log("Tomar friends er number dew")
+}
