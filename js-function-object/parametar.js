75172cdff7eac3c7d95159bbe4780effcff66313
Mehedi Hasan <mhjibon.info@gmail.com>
2023-02-06 13:44:57 +0600

    parametar js complate

---
+++ js-function-object/parametar.js
@@ -0,0 +1,17 @@
+function bringSingara(money){
+    console.log("eto takar dicen: " , money);
+    console.log("Ei nen singara");
+}
+// bringSingara(300);
+
+function add(num1, num2){
+    console.log("going to add: " , num1 , num2);
+}
+// add(125, 45);
+
+function sum(a, b, c, d, e){
+    console.log(a, b, c, d, e);
+    var sum = a + b + c + d + e;
+    console.log(sum);
+}
+sum(14, 45, 65, 34, 87);
