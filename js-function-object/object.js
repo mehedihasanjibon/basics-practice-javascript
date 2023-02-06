e1f44d4ea807afa47fe1e2969d5d59ae10327390
Mehedi Hasan <mhjibon.info@gmail.com>
2023-02-06 13:35:26 +0600

    object js complate

---
+++ js-function-object/object.js
@@ -0,0 +1,15 @@
+var student = {
+    id: 165,
+    name: "Mehedi Hasan",
+    class: "8semester",
+    marks: 3.56,
+}
+
+var mobile = {
+    brand: "infinix",
+    price: 21400,
+    storage: "128gb",
+    camera: "7mp",
+}
+
+console.log(mobile);
