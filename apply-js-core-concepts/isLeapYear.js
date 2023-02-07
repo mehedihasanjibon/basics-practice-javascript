ee24b7982f927590fcbdd5b11f2a3039a1d8278f
Mehedi Hasan <mhjibon.info@gmail.com>
2023-02-08 01:23:33 +0600

    is leap year js code done

---
+++ apply-js-core-concepts/isLeapYear.js
@@ -0,0 +1,24 @@
+// function isLeapYear(year){
+//     const remainder = year % 4;
+//     if(remainder === 0){
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+// }
+
+function isLeapYear(year){
+    const remainder = year % 4;
+    if(remainder === 0){
+        return true;
+    }
+    return false;
+}
+
+
+const isMyYearLeapYear = isLeapYear(1933);
+console.log("My year: " , isMyYearLeapYear);
+
+const isHerYearLeapYear = isLeapYear(1960);
+console.log("Her year: " , isHerYearLeapYear);
