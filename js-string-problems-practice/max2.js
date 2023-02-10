2fbd6571346283b18a21efce258d89a059e015a8
Mehedi Hasan <mhjibon.info@gmail.com>
2023-02-11 01:06:06 +0600

    max 2 some problem solve done

---
+++ js-string-problems-practice/max2.js
@@ -0,0 +1,21 @@
+const jim = 69;
+const dela = 97;
+const chinku = 99;
+
+if(jim > dela && jim > chinku){
+    console.log("jim will get eat cake in this bela");
+}
+else if(dela > jim && dela < chinku){
+    console.log("dela will eat cake in this bela");
+}
+else{
+    console.log("chinku will get the cake");
+}
+
+
+// homework: 1 
+// write a function that will take 3 numbers will return the max number 
+// write a function that will take 3 parameters and will return the min number 
+
+// first time do it using if-else
+// second time do it using Math.min or Math.mix
